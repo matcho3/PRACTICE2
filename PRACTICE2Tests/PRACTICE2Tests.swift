4d96fbe478bf7b795dc6da03eb829827bf649a89
KOHAMA TAKUYA <matcho3@KOHAMA-no-MacBook-Air-2.local>
2015-01-26 22:04:04 +0900

    Initial Commit

---
+++ PRACTICE2Tests/PRACTICE2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PRACTICE2Tests.swift
+//  PRACTICE2Tests
+//
+//  Created by KOHAMA TAKUYA on 2015/01/26.
+//  Copyright (c) 2015年 KOHAMA TAKUYA. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PRACTICE2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
